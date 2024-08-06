e877f03eaaff32b0fc14a77332c706fb57474297
Anushka Chowdhury <113542430+anushka190902@users.noreply.github.com>
2024-08-06 19:59:30 +0530

    Create LoggingAspect.java

---
+++ Week2/Spring projects/Exercise3/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(joinPoint.getSignature() + " executed in " + (endTime - startTime) + "ms");
+        return proceed;
+    }
+}
