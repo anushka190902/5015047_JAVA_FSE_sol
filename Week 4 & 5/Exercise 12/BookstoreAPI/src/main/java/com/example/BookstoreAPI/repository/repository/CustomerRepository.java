a1c61c0cf66d8409d39a25a355ed7fd29f513d0c
Anushka Chowdhury <113542430+anushka190902@users.noreply.github.com>
2024-08-28 17:49:50 +0530

    Delete Week 4 & 5/Exercise 12/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/repository directory

--- Week 4 & 5/Exercise 12/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-import com.example.BookstoreAPI.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
